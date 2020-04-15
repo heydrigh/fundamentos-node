41101f126e4f0e866cc2f69390d2fb11e2f834ad
heydrigh <heydrigh@gmail.com>
2020-04-15 19:04:13 -0300

    List, Income, Outcome

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +19,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeSum = this.transactions.reduce((total, transaction) => {
+      const { value } = transaction;
+
+      return transaction.type === 'income' ? total + value : total;
+    }, 0);
+
+    const outcomeSum = this.transactions.reduce((total, transaction) => {
+      const { value } = transaction;
+
+      return transaction.type === 'outcome' ? total + value : total;
+    }, 0);
+
+    const total = incomeSum - outcomeSum;
+
+    const balance: Balance = {
+      income: incomeSum,
+      outcome: outcomeSum,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
